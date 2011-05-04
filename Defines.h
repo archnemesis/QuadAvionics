d8289c9eb35382e8ec9b093c7d5b6ed928dbf7e2
Robin Gingras <robin@Robin-VirtualBox.(none)>
2011-05-04 10:59:28 -0400

    Removed old mavserial.h and replaced with new, updated all files to SVN head versions for switch to Git.

---
+++ Defines.h
@@ -0,0 +1,26 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define ESC_OUTPUT_1        45
+#define ESC_OUTPUT_2        44
+#define ESC_OUTPUT_3        12
+#define ESC_OUTPUT_4        13
+#define ESC_THROTTLE_MIN    1250
+#define ESC_THROTTLE_MAX    1900
+#define ESC_THROTTLE_IDLE   1350
+
+#define TLT_OUTPUT_1        0
+#define TLT_OUTPUT_2        0
+#define TLT_OUTPUT_3        0
+#define TLT_OUTPUT_4        0
+
+#define TLT_MIN_1           1000
+#define TLT_MAX 1           2000
+#define TLT_MIN_2           1000
+#define TLT_MAX 2           2000
+#define TLT_MIN_3           1000
+#define TLT_MAX 3           2000
+#define TLT_MIN_4           1000
+#define TLT_MAX 4           2000
+
+#endif
--- MavSerial.h
+++ MavSerial.h
@@ -1,23 +1,20 @@
-#ifndef mavserial_h
-#define mavserial_h
+#ifndef MAVSERIAL_H
+#define MAVSERIAL_H
 
-#define MAVLINK_USE_CONVENIENCE_FUNCTIONS
-#include "mavlink_types.h"
+#include <WProgram.h>
+#include <mavlink_types.h>
 
 mavlink_system_t mavlink_system;
-mavlink_system.sysid = 100;
-mavlink_system.compid = 200;
-
 mavlink_status_t mavlink_status;
 
 static inline void comm_send_ch(mavlink_channel_t chan, uint8_t ch)
 {
   if (chan == MAVLINK_COMM_0) {
-    Serial1.write(ch);
+    Serial.write(ch);
   }
   
   if (chan == MAVLINK_COMM_1) {
-    Serial2.write(ch);
+    Serial1.write(ch);
   }
 }
 
